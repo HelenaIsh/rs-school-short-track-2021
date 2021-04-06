9b37f7c4d145cd3ba53e6834b7d25240ed594e68
HelenaIsh <elena-ismatova@mail.ru>
2021-04-06 14:15:51 +0500

    feat: implement 10th task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,16 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let left = 0;
+  let right = array.length - 1;
+  let mid = Math.floor((left + right) / 2);
+  while (array[mid] !== value && left <= right) {
+    if (array[mid] < value) left = mid + 1;
+    else right = mid - 1;
+    mid = Math.floor((left + right) / 2);
+  }
+  return array[mid] === value ? mid : -1;
 }
 
 module.exports = findIndex;
