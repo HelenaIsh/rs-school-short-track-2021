3285530fbf15901f1bc42c629d56d321fc296f43
HelenaIsh <elena-ismatova@mail.ru>
2021-04-06 14:34:44 +0500

    feat: implement 11th task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    const letter = str[i];
+    if (i < str.length - 1 && str[i + 1] === letter) {
+      count++;
+    } else {
+      if (count === 1) {
+        result += letter;
+      } else {
+        result += `${count}${letter}`;
+      }
+      count = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
